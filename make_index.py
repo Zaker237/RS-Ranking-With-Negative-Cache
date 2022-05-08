fbcde0412fe17406dc100e96e4dd900079ecb98b
Alex Mboutchouang <mboutchouang@deken1.l3s.uni-hannover.de>
2022-05-08 14:05:27 +0200

    fix main function for making index

--- make_index.py
+++ make_index.py
@@ -1,54 +1,49 @@
 import torch
+import faiss
 import h5py
 from typing import List, Tuple
 from pathlib import Path
 from model import DualEncoder
+from transformers import AutoTokenizer
 
-MODEL_PATH = Path("/home/mboutchouang/ranking-utils/outputs/2022-04-11/11-32-20/checkpoint_epoch=16.ckpt")
+MODEL_PATH = Path("/home/mboutchouang/test_negative_cache/lightning_logs/version_4/checkpoints/epoch=15-step=353952.ckpt")
 DOCS_PATH = Path("/home/mboutchouang/ranking-utils/outputs/2022-04-11/11-32-20/data.h5")
 INDEX_PATH = Path("./indexes")
 
-class Document(object):
-    ID: int = 0
-    def __init__(self, doc: str):
-        self.ID += 1
-        self.doc_id = self.ID
-        self.doc = doc
 
+class Indexes():
+    def __init__(self, model: DualEncoder, bert_model: str):
+        self.model = model
+        self.tokenizer = AutoTokenizer.from_pretrained(bert_model)
+        self.index = faiss.IndexFlatL2(model.document_encoder.config.hidden_size)
 
-    @property
-    def query(self):
-        return self.doc
-
-
-class Indexs():
-    def __init__(self):
-        self.docs_ids = []
-        self.docs_indexes = {}
-
-    def index_documents(self, model: DualEncoder, document: Document):
-        if document.doc_id not in self.docs_ids:
-            self.docs_ids.append(document.doc_id)
-            self.docs_ids[document.doc_id] = model(document.query, document.document)
+    def load_index(self, path: Path):
+        # load the faiss index from the disk
+        self.index = faiss.read_index(str(path))
+        print("Index loaded from", path)
 
     def save_index(self, path: Path):
-        with open(path + "/docid", "w") as fp:
-            fp.write("\n".join(map(str, self.docs_ids)))
+        # save the faiss index on the disk
+        faiss.write_index(self.index, str(path))
+        print("Index saved to", path)
 
-        with open(path + "/index", "wr") as fp:
-            for doc_id in self.docs_ids:
-                fp.write("\n".join(map(str, self.docs_indexes[doc_id])))
+    def add_doc_to_index(self, doc: str):
+        doc_data = self.tokenizer([doc], padding=True, truncation=True, return_tensors="pt")
+        embedding = self.model.encode_doc(doc_data).detach().cpu().numpy()
+        self.index.add(embedding)
 
 
 def load_docs(data_file: Path) -> List[str]:
     with h5py.File(data_file, "r") as fp:
-        # queries = fp["queries"].asstr()
+        queries = fp["queries"].asstr()
         docs = fp["docs"].asstr()
-        yield Document(docs)
+        for doc in docs:
+            yield doc
 
 
 def load_model_from_checkpoint(checkpoint_path: Path) -> DualEncoder:
     model = DualEncoder.load_from_checkpoint(checkpoint_path)
+    print("Model loaded")
     return model
 
 
@@ -57,15 +52,20 @@ def main():
     # TODO: load model
     model = load_model_from_checkpoint(MODEL_PATH)
 
-    # Init the index
-
-    indexes = Indexs()
+    # Init the index encoder
+    index = Indexes(model, "bert-base-uncased")
+    print("Index initialized", index.index.is_trained)
 
     # TODO: load data and build index
+    i = 0
     for document in load_docs(DOCS_PATH):
-        indexes.index_documents(model, document)
+        print(f"Adding document {i}")
+        index.add_doc_to_index(document)
+        i += 1
 
-    indexes.save_index(INDEX_PATH)
+    # the Inedex is now ready to be used and saved
+    index.save_index(INDEX_PATH)
+
 
 if __name__ == "__main__":
     main()
