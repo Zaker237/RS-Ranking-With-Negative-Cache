79d5b7736617b6102b62e86e0964b1db894bf8ea
Alex Mboutchouang <mboutchouang@deken1.l3s.uni-hannover.de>
2022-05-11 16:06:25 +0200

    update the index script load all doc on the CPU

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+from pathlib import Path
+
+MODEL_PATH = Path("/home/mboutchouang/test_negative_cache/lightning_logs/version_4/checkpoints/epoch=15-step=353952.ckpt")
+DOCS_PATH = Path("/home/mboutchouang/ranking-utils/outputs/2022-04-11/11-32-20/data.h5")
+INDEX_PATH = Path("./indexes")
